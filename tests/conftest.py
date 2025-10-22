61d20aa0c14538dbe15d5c7d50c6a2151f2376d0
Christian Hogan <1cph93@gmail.com>
2025-10-22 19:10:06 -0400

    Move shared test fixtures to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pytest
+from _pytest.fixtures import SubRequest  # NOTE: Deprecated
+from pydantic import HttpUrl
+
+from rely.core.models.repo_identifier import RepoIdentifier
+from rely.clients.models.full_repository import FullRepository
+from rely.clients.models.content_tree_list import ContentTreeList
+
+
+@pytest.fixture
+def local_test_dir(request: SubRequest) -> Path:
+    """Get the path of the current testing directory."""
+
+    return Path(request.node.fspath).parent
+
+
+@pytest.fixture
+def repo_identifier() -> RepoIdentifier:
+    """Fixture to create a RepoIdentifier."""
+
+    return RepoIdentifier(url=HttpUrl("https://github.com/1cph93/rely"))
+
+
+@pytest.fixture
+def full_repository(local_test_dir: Path) -> FullRepository:
+    """Fixture to create a FullRepository.  Loads content from a local JSON file."""
+
+    file_path = local_test_dir / "full_repository.json"
+
+    with open(file_path.resolve(), "r") as in_file:
+        file_content = in_file.read()
+        return FullRepository.model_validate_json(file_content)
+
+
+@pytest.fixture
+def content_tree_list(local_test_dir: Path) -> ContentTreeList:
+    """Fixture to create a ContentTreeList.  Loads content from a local JSON file."""
+
+    file_path = local_test_dir / "content_tree_list.json"
+
+    with open(file_path.resolve(), "r") as in_file:
+        file_content = in_file.read()
+        return ContentTreeList.model_validate_json(file_content)
--- tests/core/models/test_repo_context/test_repo_context.py
+++ tests/core/models/test_repo_context/test_repo_context.py
@@ -1,10 +1,7 @@
-from pathlib import Path
 from unittest import mock
 
 import pytest
-from _pytest.fixtures import SubRequest  # NOTE: Deprecated
 from pytest_mock import MockerFixture
-from pydantic import HttpUrl
 
 from rely.clients.http_client import HTTPClient
 from rely.clients.github_api_client import GitHubAPIClient
@@ -12,42 +9,6 @@ from rely.core.models.repo_identifier import RepoIdentifier
 from rely.core.models.repo_context import RepoContext, create_repo_context
 from rely.clients.models.full_repository import FullRepository
 from rely.clients.models.content_tree_list import ContentTreeList
-
-
-@pytest.fixture
-def local_test_dir(request: SubRequest) -> Path:
-    """Get the path of the current testing directory."""
-
-    return Path(request.node.fspath).parent
-
-
-@pytest.fixture
-def repo_identifier() -> RepoIdentifier:
-    """Fixture to create a RepoIdentifier."""
-
-    return RepoIdentifier(url=HttpUrl("https://github.com/1cph93/rely"))
-
-
-@pytest.fixture
-def full_repository(local_test_dir: Path) -> FullRepository:
-    """Fixture to create a FullRepository.  Loads content from a local JSON file."""
-
-    file_path = local_test_dir / "full_repository.json"
-
-    with open(file_path.resolve(), "r") as in_file:
-        file_content = in_file.read()
-        return FullRepository.model_validate_json(file_content)
-
-
-@pytest.fixture
-def content_tree_list(local_test_dir: Path) -> ContentTreeList:
-    """Fixture to create a ContentTreeList.  Loads content from a local JSON file."""
-
-    file_path = local_test_dir / "content_tree_list.json"
-
-    with open(file_path.resolve(), "r") as in_file:
-        file_content = in_file.read()
-        return ContentTreeList.model_validate_json(file_content)
 
 
 @pytest.fixture
