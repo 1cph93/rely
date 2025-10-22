f0a4f60cafdf32fc52d7874d2e17484c3611bdcf
Christian Hogan <1cph93@gmail.com>
2025-10-22 10:39:25 -0400

    Improve BaseMetric method descriptions

--- rely/core/metrics/base_metric.py
+++ rely/core/metrics/base_metric.py
@@ -28,7 +28,10 @@ class BaseMetric(ABC):
 
     @classmethod
     def __init_subclass__(cls) -> None:
-        """Update registry every time BaseMetric is subclassed."""
+        """
+        Update registry every time BaseMetric is subclassed.
+        Provides a simple way to keep track of all metric subclasses.
+        """
 
         super().__init_subclass__()
         cls._registry[cls.get_normalized_name()] = cls
@@ -49,7 +52,7 @@ class BaseMetric(ABC):
 
     @classmethod
     def get_prettified_name(cls) -> str:
-        """Get prettified metric name."""
+        """Get prettified (human-readable) metric name."""
 
         _, prettified_name = cls.metric_name.value
 
@@ -58,26 +61,38 @@ class BaseMetric(ABC):
     @functools.cache
     @abstractmethod
     def compute_metric_value(self) -> MetricValue:
-        """Compute metric value."""
+        """
+        Compute raw metric value.
+        NOTE: This is an unaltered value that comes from the repo itself,
+              like number of stars or time since last commit.
+        """
 
         pass
 
     @functools.cache
     @abstractmethod
     def compute_metric_score(self) -> MetricScore:
-        """Compute metric score."""
+        """
+        Compute metric score.
+        NOTE: This is a score that we assign based on the metric's value,
+              like poor, average, or good.
+        """
 
         pass
 
     def compute_metric_weighted_score(self) -> Decimal:
-        """Compute metric weighted score."""
+        """
+        Compute metric weighted score.
+        NOTE: We assign a weight to each metric to indicate how important it is.
+              The weigted score is equal to weight * score.
+        """
 
         metric_score = self.compute_metric_score().value
 
         return Decimal(metric_score) * self.metric_weight
 
     def serialize(self) -> SerializedMetric:
-        """Serialize computed model fields."""
+        """Serialize computed metric fields."""
 
         return SerializedMetric(
             normalized_name=self.get_normalized_name(),
