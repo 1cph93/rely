01a8c1aa6ea32d600b56aaead7ef9b0c7fde6c43
Christian Hogan <1cph93@gmail.com>
2025-10-22 19:45:25 -0400

    Create shared repo_context test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from _pytest.fixtures import SubRequest  # NOTE: Deprecated
 from pydantic import HttpUrl
 
 from rely.core.models.repo_identifier import RepoIdentifier
+from rely.core.models.repo_context import RepoContext
 from rely.clients.models.full_repository import FullRepository
 from rely.clients.models.content_tree_list import ContentTreeList
 
@@ -43,3 +44,16 @@ def content_tree_list(local_test_dir: Path) -> ContentTreeList:
     with open(file_path.resolve(), "r") as in_file:
         file_content = in_file.read()
         return ContentTreeList.model_validate_json(file_content)
+
+
+@pytest.fixture
+def repo_context(
+    repo_identifier: RepoIdentifier,
+    full_repository: FullRepository,
+    content_tree_list: ContentTreeList,
+):
+    return RepoContext(
+        repo_identifier=repo_identifier,
+        full_repository=full_repository,
+        content_tree_list=content_tree_list,
+    )
