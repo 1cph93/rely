9876d9c36275d5929c6b13559dd8c8678171d4e0
Christian Hogan <1cph93@gmail.com>
2025-10-22 11:20:09 -0400

    Create MetricReducer class

---
+++ rely/core/metrics/metric_reducer.py
@@ -0,0 +1,56 @@
+from decimal import Decimal
+from typing import Type, Collection
+import functools
+
+from rely.core.models.repo_context import RepoContext
+from rely.core.metrics.types import MetricScore
+from rely.core.metrics.base_metric import BaseMetric
+
+
+class MetricReducer:
+    """Compute overall values for a collection of metrics."""
+
+    def __init__(
+        self, metric_classes: Collection[Type[BaseMetric]], repo_context: RepoContext
+    ) -> None:
+        self._metric_classes = metric_classes
+        self._repo_context = repo_context
+
+    @property
+    def _metric_instances(self) -> list[BaseMetric]:
+        """Create a list of metric instances."""
+
+        return [
+            metric_class(self._repo_context) for metric_class in self._metric_classes
+        ]
+
+    @property
+    def _highest_possible_score(self) -> Decimal:
+        """Compute the highest possible score that a repo can have."""
+
+        highest_possible_score = MetricScore.GOOD.value * len(self._metric_classes)
+
+        return Decimal(highest_possible_score)
+
+    @functools.cache
+    def _compute_weighted_sum(self) -> Decimal:
+        """Compute the sum of all weighted scores."""
+
+        weighted_sum = sum(
+            [
+                metric_instance.compute_metric_weighted_score()
+                for metric_instance in self._metric_instances
+            ]
+        )
+
+        return Decimal(weighted_sum)
+
+    def compute_overall_score(self) -> Decimal:
+        """
+        Compute the overall score for a repo.
+        NOTE: This is defined as weighted sum / hightest possible score
+        """
+
+        weighted_sum = self._compute_weighted_sum()
+
+        return weighted_sum / self._highest_possible_score
