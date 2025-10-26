95cdeb65418a7a768a54bdb90abc24af5245120c
Christian Hogan <1cph93@gmail.com>
2025-10-26 14:35:58 -0400

    Define SUPPORTED_README_EXTENSIONS constant

---
+++ rely/config/constants.py
@@ -0,0 +1,22 @@
+# Supported README extensions: https://github.com/github/markup/blob/master/README.md#markups
+SUPPORTED_README_EXTENSIONS = frozenset(
+    {
+        "markdown",
+        "mdown",
+        "mkdn",
+        "md",
+        "textile",
+        "rdoc",
+        "org",
+        "creole",
+        "mediawiki",
+        "wiki",
+        "rst",
+        "asciidoc",
+        "adoc",
+        "asc",
+        "pod",
+        # Also support txt
+        "txt",
+    }
+)
