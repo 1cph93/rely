69dc56b3eeafe9e03c542165f97202b4235160aa
Christian Hogan <1cph93@gmail.com>
2025-10-26 14:37:16 -0400

    Ensure README file path is valid in HasReadmeMetric

--- rely/core/metrics/has_readme_metric.py
+++ rely/core/metrics/has_readme_metric.py
@@ -1,6 +1,7 @@
 import functools
 from decimal import Decimal
 
+from rely.config.constants import SUPPORTED_README_EXTENSIONS
 from rely.core.metrics.base_metric import BaseMetric
 from rely.core.metrics.types import MetricName, MetricValue, MetricScore
 
@@ -16,9 +17,16 @@ class HasReadmeMetric(BaseMetric):
         """Return True if repo has non-empty README in root directory, otherwise return False."""
 
         content_tree_list = self.repo_context.content_tree_list
+        possible_readme_paths = {
+            f"readme.{extension}" for extension in SUPPORTED_README_EXTENSIONS
+        }
+        # Include README with no extension to possible paths
+        possible_readme_paths.add("readme")
 
         return any(
-            content_tree.type == "file" and content_tree.size > 0
+            content_tree.type == "file"
+            and content_tree.path.lower() in possible_readme_paths
+            and content_tree.size > 0
             for content_tree in content_tree_list.content_tree_list
         )
 
